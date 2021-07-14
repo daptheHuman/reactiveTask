81431329f93cbc3141ed254a9074575e61b4f802
Dap <dafaaqilla72@gmail.com>
2021-07-14 18:53:11 +0700

    render the filtered state instead initial state

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,40 +1,44 @@
 import React from 'react';
+import './Todo.css';
 
+const Todo = ({ taskName, taskComplete, todo, todos, setTodos }) => {
+	const deleteHandler = () => {
+		//Its creating a new list, its iterating through the array and removing the elements that equal todo.id filter-logic.png-
+		const newList = todos.filter((items) => items.id !== todo.id);
+		//Setting the new list to the setTodos prop
+		setTodos(newList);
+		console.log('Deleting task..');
+	};
 
-const Todo = ({keys,taskName,taskComplete,todo, todos,setTodos}) => {
+	const completeHandler = () => {
+		//Iterating array todos with map and if the name equals to the item task make the complete to opposite value
+		setTodos(
+			todos.map((item) => {
+				if (item.task === todo.task) {
+					return {
+						...item,
+						complete: !item.complete,
+					};
+				}
+				return item;
+			}),
+		);
+	};
 
-    const deleteHandler = (id) => {
-        //Its creating a new list, its iterating through the array and removing the elements that equal todo.id filter-logic.png-
-        const newList= todos.filter(items => items.id !== todo.id)
-        //Setting the new list to the setTodos prop
-        setTodos(newList);
-        console.log('Deleting task..');
-    }
-
-    const completeHandler = () => {
-        //Iterating array todos with map and if the name equals to the item task make the complete to opposite value
-        setTodos(todos.map(item => {
-            if (item.task === todo.task) {
-                return {
-                    ...item,complete: !item.complete}
-            }
-            return item;
-        }));    
-    }
-    return (
-        <li className="list-group">
-            <i className="fa fa-pencil"></i>
-            <span className={taskComplete+" item"}>{taskName}</span>
-            <div className="actions">
-            <button onClick={completeHandler} className="complete-btn">
-                <i className="fa fa-check"></i>    
-            </button>
-            <button onClick={() => deleteHandler(keys)} className="delete-btn">
-                <i className="fa fa-times"></i>
-            </button>
-            </div>
-        </li>
-    );
-}
+	return (
+		<li className="Todo list-group">
+			<i className="fa fa-pencil"></i>
+			<span className={taskComplete + ' item'}>{taskName}</span>
+			<div className="actions">
+				<button onClick={completeHandler} className="complete-btn">
+					<i className="fa fa-check"></i>
+				</button>
+				<button onClick={deleteHandler} className="delete-btn">
+					<i className="fa fa-times"></i>
+				</button>
+			</div>
+		</li>
+	);
+};
 
 export default Todo;
--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -4,24 +4,25 @@ import './TodoList.css';
 //Import Components
 import Todo from './Todo';
 
-const TodoList = ({todos,setTodos}) => {
-    return (    
-        <div className="TodoList">
-            <ul className="List">
-                {todos.map(todo =>(
-                    <Todo 
-                    todos={todos}
-                    setTodos={setTodos}
-                    todo={todo}
-                    keys={todo.id}
-                    taskName={todo.task}
-                    taskComplete={todo.complete ? "strike" : ""}
-                    // true = done, false = not done
-                    />      
-                    ))}
-            </ul>
-        </div>
-    )
-}
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+	return (
+		<div className="TodoList">
+			{/* Render Todos */}
+			<ul className="List">
+				{filteredTodos.map((todo) => (
+					<Todo
+						key={todo.id}
+						todos={todos}
+						setTodos={setTodos}
+						todo={todo}
+						taskName={todo.task}
+						taskComplete={todo.complete ? 'strike' : ''}
+						// true = done, false = not done
+					/>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default TodoList;
